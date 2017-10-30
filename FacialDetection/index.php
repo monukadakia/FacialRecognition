7df01579baddfa690f2c28de94b41f08dd5f336a
Mohnish Kadakia <monukadakia@gmail.com>
2017-10-30 14:47:33 -0700

    added new folders for video_in and video_out

--- FacialDetection/index.php
+++ FacialDetection/index.php
@@ -3,15 +3,15 @@
  	header("Access-Control-Allow-Origin: *");
  	//var_dump($_ENV["HOME"]);
  	$filePath = $_GET["fileInfo"];
- 	if (!file_exists('/Applications/MAMP/htdocs/FaceDetection/videos/')) {
- 	  mkdir('/Applications/MAMP/htdocs/FaceDetection/videos/', 0777, true);
+ 	if (!file_exists('/Applications/MAMP/htdocs/FacialRecognition/FacialDetection/video_in/')) {
+ 	  mkdir('/Applications/MAMP/htdocs/FacialRecognition/FacialDetection/video_in/', 0777, true);
  	}
- 	$filePath = '/Applications/MAMP/htdocs/FaceDetection/videos/'.$filePath;
+ 	$filePath = '/Applications/MAMP/htdocs/FacialRecognition/FacialDetection/video_in/'.$filePath;
  	$command = '/usr/local/bin/ffprobe -v error -count_frames -select_streams v:0 -show_entries stream=nb_read_frames -of default=nokey=1:noprint_wrappers=1 '.$filePath;
  	echo exec($command, $output, $return_var);
- 	if (!file_exists('/Applications/MAMP/htdocs/FaceDetection/transcoded/')) {
- 	  mkdir('/Applications/MAMP/htdocs/FaceDetection/transcoded/', 0777, true);
+ 	if (!file_exists('/Applications/MAMP/htdocs/FacialRecognition/FacialDetection/transcoded/')) {
+ 	  mkdir('/Applications/MAMP/htdocs/FacialRecognition/FacialDetection/transcoded/', 0777, true);
  	}
- 	$command = '/usr/local/bin/ffmpeg  -i '.$filePath.' -vf fps=30 /Applications/MAMP/htdocs/FaceDetection/transcoded/image-%03d.png';
+ 	$command = '/usr/local/bin/ffmpeg  -i '.$filePath.' -vf fps=30 /Applications/MAMP/htdocs/FacialRecognition/FacialDetection/transcoded/image-%03d.png';
   echo shell_exec($command);
   ?>
